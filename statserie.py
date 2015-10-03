df30799d595d21bc5a2c442ec4bd5d4108c109ce
LudoBike <ludovicdruette@gmail.com>
2015-10-03 16:08:39 +0200

    Change Statserie to respect the standard

--- statserie.py
+++ statserie.py
@@ -1,40 +1,40 @@
 class StatSerie(object):
     def __init__(self, serie):
         try:
-            self.serie = [float(i) for i in serie]
+            self._serie = [float(i) for i in serie]
         except ValueError:
             print("Error: no numeric variable in list")
         else:
-            serie.sort()
+            self._serie.sort()
             self.refresh()
 
     def __repr__(self):
-        to_return = "; ".join(str(x) for x in self.serie)
+        to_return = "; ".join(str(x) for x in self._serie)
         to_return += "\nétendue = {0}; mediane = {1}; q1 = {2}; q2 = {3}; moyenne = {4}"\
-            .format(self.etendue, self.mediane, self.q1, self.q3, self.moyenne)
+            .format(self._etendue, self._mediane, self._q1, self._q3, self._moyenne)
         return to_return
 
     def cal_etendue(self):
-        self.etendue = max(self.serie) - min(self.serie)
+        self._etendue = max(self._serie) - min(self._serie)
 
     def cal_mediane(self):
-        nbr_valeur = len(self.serie)
+        nbr_valeur = len(self._serie)
         if (nbr_valeur%2):
-            self.mediane = self.serie[nbr_valeur//2]
+            self._mediane = self._serie[nbr_valeur//2]
         else:
-            self.mediane = (self.serie[nbr_valeur//2] + \
-                            self.serie[nbr_valeur//2 - 1])/2
+            self._mediane = (self._serie[nbr_valeur//2] + \
+                            self._serie[nbr_valeur//2 - 1])/2
 
     def cal_q1(self):
-        q1_index = len(self.serie)//4
-        self.q1 = self.serie[q1_index]
+        q1_index = len(self._serie)//4
+        self._q1 = self._serie[q1_index]
 
     def cal_q3(self):
-        q3_index = len(self.serie)//4*3
-        self.q3 = self.serie[q3_index]
+        q3_index = len(self._serie)//4*3
+        self._q3 = self._serie[q3_index]
 
     def cal_moyenne(self):
-        self.moyenne = sum(self.serie)/len(self.serie)
+        self._moyenne = sum(self._serie)/len(self._serie)
 
     def refresh(self):
         self.cal_etendue()
@@ -45,13 +45,13 @@ class StatSerie(object):
 
     def append(self, to_append):
         try:
-            self.serie.append(float(to_append))
+            self._serie.append(float(to_append))
         except ValueError:
             print("error : no numeric variable")
         else:
-            self.serie.sort()
+            self._serie.sort()
             self.refresh()
-            
+
     def save(self, save_file_path):
         if (type(save_file_path) != str):
             print("Bad argument please enter string")
